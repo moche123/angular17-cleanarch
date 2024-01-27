784919388d519d143f50679c92039fb5dd27cb7b
moche123 <mochemiguel@gmail.com>
2024-01-26 19:07:05 -0500

    feature[HU-4]: Add basic material config to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from "@angular/material/card";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'project';
-}
+export class AppComponent {}
